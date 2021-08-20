782f2db972b030573f3f72f586114e6ba84f8671
merinosilva <merino.silva@gmail.com>
2021-08-21 00:19:08 +0530

    add angular component

--- client-app-ng/src/app/app.module.ts
+++ client-app-ng/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CompanyDataFormComponent } from './company-data-form/company-data-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyDataFormComponent
   ],
   imports: [
     BrowserModule
---
+++ client-app-ng/src/app/company-data-form/company-data-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-data-form',
+  templateUrl: './company-data-form.component.html',
+  styleUrls: ['./company-data-form.component.css']
+})
+export class CompanyDataFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
